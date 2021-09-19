2e907650e8f8fe960afe3f739f5139db820ee966
ankit <ac2000choudhary@gmail.com>
2021-09-19 21:24:36 +0530

    students

---
+++ student/index.php
@@ -0,0 +1,27 @@
+<?php
+include("./header.php");
+?>
+
+<body>
+    <main class="login">
+        <div class="login-admin-card">
+            <img src="/assets/img/login.png">
+            <form class="login-form">
+                <img src="/assets/img/Student_profile.png">
+                <div class="form-field">
+                    <input type="email" placeholder="Email">
+                </div>
+                <div class="form-field">
+                    <input type="password" placeholder="Password">
+                </div>
+                <div class="form-field">
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
+    </main>
+</body>
+
+<?php
+include('./footer.php');
+?>
