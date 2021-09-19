5eaac9ce4ed91e9bf7a94c7f4412ced226b4f537
ankit <ac2000choudhary@gmail.com>
2021-09-19 21:21:17 +0530

    index is created

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMS</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <main>
+        <lable>Student Management System</lable>
+        <div class="login-component">
+            <div class="login-card">
+                <img src="/assets/img/admin_profile.png">
+                <a href="./admin/">Login as Admin</a>
+            </div>
+            <div class="login-card">
+                <img src="/assets/img/Student_profile.png">
+                <a href="./student/">Click here to see the Result</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
