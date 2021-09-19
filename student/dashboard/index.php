05ffebd1476b77162b57939558a7080b3fae8dd5
ankit <ac2000choudhary@gmail.com>
2021-09-19 21:25:03 +0530

    student dashboard

---
+++ student/dashboard/index.php
@@ -0,0 +1,35 @@
+<?php
+include("../header.php");
+?>
+
+<body>
+    <main class="admin">
+        <nav>
+           <a class="navbar-title">
+                <i class="fas fa-address-card"></i><span>SMS</span>
+            </a>
+
+           <div class="navbar-menu">
+                <div class="menu-item">
+                    <i class="fas fa-tachometer-alt"></i><span>Dashboard</span>
+                </div>
+                <div class="menu-item">
+                    <i class="fas fa-archway"></i><span>Classes</span>
+                </div>
+                <div class="menu-item">
+                    <i class="fa fa-users" aria-hidden="true"></i><span>Students</span>
+                </div>
+                <div class="menu-item">
+                    <i class="fas fa-poll-h"></i><span>Results</span>
+                </div>
+                <div class="menu-item">
+                    <i class="fa fa-sign-out" aria-hidden="true"></i><span>Log Out</span>
+                </div>
+           </div>
+        </nav>
+    </main>
+</body>
+
+<?php
+include('../footer.php');
+?>
