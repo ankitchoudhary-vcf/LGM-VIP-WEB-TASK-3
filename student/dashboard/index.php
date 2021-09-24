5c0345202e627b064e767b03b328691284b8f79a
ankit <ac2000choudhary@gmail.com>
2021-09-24 12:39:13 +0530

    dashboard updated

--- student/dashboard/index.php
+++ student/dashboard/index.php
@@ -1,32 +1,67 @@
 <?php
 include("../header.php");
+require_once('../../database/Class.php');
+require_once('../../database/Students.php');
 ?>
 
 <body>
     <main class="admin">
-        <nav>
-           <a class="navbar-title">
-                <i class="fas fa-address-card"></i><span>SMS</span>
-            </a>
+        <div class="subjects">
+            <div class="add-subject">
+                <div class="title">
+                    <?php
+                        $students = new Students();
+                        $name = $students->getStudentNameByClassnoAndrollno($_GET['classno'],$_GET['rollno']);
+                        echo "Result of ". $name[0]['name'];
+                    ?>
+                </div>
+                
+                <div class="subjects-details" style="width: 80%;">
+                    <div class="title">Total Marks</div>
+                    <?php
+                        require_once('../../database/Marks.php');
+                        require_once('../../database/Subjects.php');
+                        $marks = new Marks();
+                        $subjects = new Subjects();
+                        
+                        $classno = $_GET['classno'];
+                        $rollno = $_GET['rollno'];
+                        
+                        $numberofsubjects = $subjects->getNumberOfSubjects($classno);
+                        $total = $marks->getTotalMarks($rollno, $classno);
+                        $all = $numberofsubjects * 100;
+                        echo "$total out of $all"
 
-           <div class="navbar-menu">
-                <div class="menu-item">
-                    <i class="fas fa-tachometer-alt"></i><span>Dashboard</span>
+                    ?>
                 </div>
-                <div class="menu-item">
-                    <i class="fas fa-archway"></i><span>Classes</span>
+
+                <div class="subjects-details" style="width: 80%;">
+                    <div class="title">Percentage</div>
+                    <?php         
+                        echo $total / $numberofsubjects . " %";
+                    ?>
                 </div>
-                <div class="menu-item">
-                    <i class="fa fa-users" aria-hidden="true"></i><span>Students</span>
+
+                <div class="subjects-details" style="width: 80%;">
+                    <div class="title">Subject vise marks</div>
+
+                    <?php
+                        $data = $marks->getMarks($_GET['rollno'], $_GET['classno']);
+                        foreach ($data as $row) {
+                            echo "<div class='subjects-container'>";
+                            echo "<div class='subject-item'>";
+                            echo $row['subject'];
+                            echo "</div>";
+                            echo "<div class='subject-item'>";
+                            echo '<strong>' . $row['marks'] . '</strong>';
+                            echo "</div>";
+                            echo "</div>";
+                        }
+
+                    ?>
                 </div>
-                <div class="menu-item">
-                    <i class="fas fa-poll-h"></i><span>Results</span>
-                </div>
-                <div class="menu-item">
-                    <i class="fa fa-sign-out" aria-hidden="true"></i><span>Log Out</span>
-                </div>
-           </div>
-        </nav>
+            </div>
+        </div>
     </main>
 </body>
 
