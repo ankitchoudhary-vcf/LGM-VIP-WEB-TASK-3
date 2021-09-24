0141fa408aec25bda339632a60d298416bf494ae
ankit <ac2000choudhary@gmail.com>
2021-09-24 12:39:00 +0530

    student updated

--- student/index.php
+++ student/index.php
@@ -1,21 +1,39 @@
 <?php
 include("./header.php");
+
+if (isset($_POST['check'])) {
+    $classno=$_POST['classno'];
+    $rollno = $_POST['rollno'];
+    header('location:/student/dashboard/?classno='.$classno.'&rollno='.$rollno.'');
+}
 ?>
 
 <body>
     <main class="login">
         <div class="login-admin-card">
             <img src="/assets/img/login.png">
-            <form class="login-form">
+            <form class="login-form" method="post">
                 <img src="/assets/img/Student_profile.png">
                 <div class="form-field">
-                    <input type="email" placeholder="Email">
+                    <select name="classno" required>
+                        <option value="Select Class">Select Class</option>
+                        <?php 
+                            require_once('../database/Class.php');
+                            require_once('../database/Students.php');
+                            $classes = new Classes();
+                            $students = new Students();
+                            $data = $classes->getAllClasses();
+                            foreach ($data as $class) {
+                                echo "<option value='" . $class['classno'] . "'>" . $class['classno'] . "</option>";
+                            }
+                        ?>
+                    </select>
                 </div>
                 <div class="form-field">
-                    <input type="password" placeholder="Password">
+                    <input type="number" name="rollno" placeholder="Roll no" required>
                 </div>
                 <div class="form-field">
-                    <button type="submit">Login</button>
+                    <button type="submit" name="check">Check Result</button>
                 </div>
             </form>
         </div>
